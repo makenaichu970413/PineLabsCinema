8759b04a571f06680fb43893bf6480233a164653
makenaichuweb <makenaichuweb@gmail.com>
2022-06-07 18:49:59 +0800

    7th commit

--- next.config.js
+++ next.config.js
@@ -4,19 +4,19 @@ module.exports = () => {
   return {
     reactStrictMode: true,
     i18n,
+
     env: {
       domain: "https://pinelabscinema.vercel.app",
-
       movieDB: {
         apiDomain: "https://api.themoviedb.org/3",
         imgDomain: "https://image.tmdb.org/t/p/original",
         apiKey: "328c283cd27bd1877d9080ccb1604c91",
       },
-
       redirect: "https://www.cathaycineplexes.com.sg/",
     },
+
     images: {
-      domains: ["image.tmdb.org"],
+      domains: ["image.tmdb.org", "pinelabscinema.vercel.app"],
     },
   };
 };
